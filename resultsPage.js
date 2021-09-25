e527cd0fe960165f78460f586a25f1b2ba2ef52e
leahlondon <leahtoprop@gmail.com>
2021-09-25 20:23:18 +0300

    adding results page - halfway

--- resultsPage.js
+++ resultsPage.js
@@ -1,7 +1,8 @@
 class Results {
     constructor() {
-        this.results = this.parseResults();
+        this.data = this.parseResults();
         this.currentResult = this.getCurrentResult();
+        this.headers = ['Plater Name', 'Score', 'Time'];
     }
 
     parseResults() {
@@ -16,8 +17,8 @@ class Results {
     }
 
     saveCurrentResult(playerName) {
-        this.results.push({ 'playerName': playerName, 'result': this.currentResult, 'time': new Date() })
-        window.localStorage.setItem('results', JSON.stringify(this.results));
+        this.data.push({ 'playerName': playerName, 'result': this.currentResult, 'time': new Date() })
+        window.localStorage.setItem('results', JSON.stringify(this.data));
     }
 }
 
@@ -26,6 +27,8 @@ class Display {
         this.saveResultButton = document.querySelector('#saveResult');
         this.resultDisplay = document.querySelector('#resultDisplay');
         this.playerNameInput = document.querySelector('#playerName');
+        this.resultsTable = document.querySelector('#resultsTable');
+        this.allResultsHeader = document.querySelector('#allResultsHeader');
         this.results = new Results();
     }
     initiate() {
@@ -37,11 +40,36 @@ class Display {
         this.saveResultButton.addEventListener('click', () => {
             this.results.saveCurrentResult(this.playerNameInput.value);
             this.saveResultButton.disabled = true;
+            this.buildResultsTable();
         });
     }
     setResultDisplay() {
         this.resultDisplay.innerText = this.results.currentResult;
     }
+
+    buildResultsTable() {
+        this.allResultsHeader.style.display = 'block';
+        // table header
+        let tr = document.createElement('tr');
+        for (let header of this.results.headers) {
+            let th = document.createElement('th');
+            th.innerText = header;
+            tr.appendChild(th);
+        }
+        this.resultsTable.appendChild(tr);
+
+        // data
+        for (let line of this.results.data) {
+            let tr = document.createElement('tr');
+            for (let e of Object.values(line)) {
+                let td = document.createElement('td');
+                td.innerText = e;
+                tr.appendChild(td);
+            }
+            this.resultsTable.appendChild(tr);
+        }
+    }
 }
+
 const display = new Display();
 display.initiate();
